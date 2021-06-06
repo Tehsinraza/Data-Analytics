4ceab871d3c2a57486b5b8bbc49cb8803b121d3d
Tehsinraza <85433897+Tehsinraza@users.noreply.github.com>
2021-06-06 14:56:03 +0530

    program-0

---
+++ temp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x = [1,2,3,4,5]
+y = [2,4,6,8,10]
+print (x)
+print (y)
+import matplotlib.pyplot as plt
+plt.plot(x,y)
